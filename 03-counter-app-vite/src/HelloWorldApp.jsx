2559aa048f49270b1b76d974a4922c8375e99eea
GermanDanielRivera <germandanielriveramejia1@gmail.com>
2025-05-07 17:19:08 -0600

    03-counter-app-vite

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+/*
+function App() {
+    return (<h1>Hola Mundo!!!</h1>);
+}
+
+export default App;*/
+
+
+//export const HelloWorldApp = () => <h1>Arrow Function</h1>;
+
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  );
+}
+
+
