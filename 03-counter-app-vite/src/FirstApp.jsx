6641be3054bea33d20c79d49da61bc0810a7dcdf
GermanDanielRivera <germandanielriveramejia1@gmail.com>
2025-05-09 17:16:41 -0600

    03-counter-app-vite

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
 
   return ( 
     <>
         {/*<code>{JSON.stringify(newMessage)}</code>*/}
         <h1>{title}</h1>
         <p>{subTitle}</p>
+        <p>{name}</p>
     </>
   )
 }
